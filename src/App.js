84bc51b97121ca20f08489b4d4622ff031301d7c
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-24 18:32:30 +0100

    Practiced Event Listeners in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  function handleClick() {
+    console.log("I was clicked!");
+  }
+  function hover() {
+    console.log("I was hovered");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img onMouseOver={hover} src="https://picsum.photos/640/360" />
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
-
-export default App;
